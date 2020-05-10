3608fa368121b1475d78d02ba93e6faf89be26fb
Ilya Mateyko <me@astrophena.me>
2020-05-10 18:08:11 +0300

    simplify

--- main.go
+++ main.go
@@ -19,51 +19,51 @@ var Version = "dev"
 func main() {
 	log.SetFlags(0)
 
-	app := cli.NewApp()
-
-	app.Name = "cloudshell"
-	app.Usage = "Manage Google Cloud Shell."
-	app.EnableBashCompletion = true
-	app.Version = Version
-	app.HideHelpCommand = true
-	app.Commands = []*cli.Command{
-		&cli.Command{
-			Name:    "connect",
-			Aliases: []string{"c"},
-			Usage:   "Establish an interactive SSH session with Cloud Shell",
-			Action:  commands.Connect,
-		},
-		&cli.Command{
-			Name:    "info",
-			Aliases: []string{"i"},
-			Usage:   "Print information about the environment",
-			Action:  commands.Info,
-		},
-		&cli.Command{
-			Name:            "key",
-			Aliases:         []string{"k"},
-			Usage:           "Manage public keys associated with the Cloud Shell",
-			HideHelpCommand: true,
-			Subcommands: []*cli.Command{
-				&cli.Command{
-					Name:    "list",
-					Aliases: []string{"l"},
-					Usage:   "List public keys associated with the Cloud Shell",
-					Action:  commands.KeyList,
-				},
-				&cli.Command{
-					Name:      "add",
-					Aliases:   []string{"a"},
-					Usage:     "Add a public SSH key to the Cloud Shell",
-					ArgsUsage: "[key format] [key]",
-					Action:    commands.KeyAdd,
-				},
-				&cli.Command{
-					Name:      "delete",
-					Aliases:   []string{"d"},
-					Usage:     "Remove a public SSH key from the Cloud Shell",
-					ArgsUsage: "[key id]",
-					Action:    commands.KeyDelete,
+	app := &cli.App{
+		Name:                 "cloudshell",
+		Usage:                "Manage Google Cloud Shell.",
+		EnableBashCompletion: true,
+		Version:              Version,
+		HideHelpCommand:      true,
+		Commands: []*cli.Command{
+			&cli.Command{
+				Name:    "connect",
+				Aliases: []string{"c"},
+				Usage:   "Establish an interactive SSH session with Cloud Shell",
+				Action:  commands.Connect,
+			},
+			&cli.Command{
+				Name:    "info",
+				Aliases: []string{"i"},
+				Usage:   "Print information about the environment",
+				Action:  commands.Info,
+			},
+			&cli.Command{
+				Name:            "key",
+				Aliases:         []string{"k"},
+				Usage:           "Manage public keys associated with the Cloud Shell",
+				HideHelpCommand: true,
+				Subcommands: []*cli.Command{
+					&cli.Command{
+						Name:    "list",
+						Aliases: []string{"l"},
+						Usage:   "List public keys associated with the Cloud Shell",
+						Action:  commands.KeyList,
+					},
+					&cli.Command{
+						Name:      "add",
+						Aliases:   []string{"a"},
+						Usage:     "Add a public SSH key to the Cloud Shell",
+						ArgsUsage: "[key format] [key]",
+						Action:    commands.KeyAdd,
+					},
+					&cli.Command{
+						Name:      "delete",
+						Aliases:   []string{"d"},
+						Usage:     "Remove a public SSH key from the Cloud Shell",
+						ArgsUsage: "[key id]",
+						Action:    commands.KeyDelete,
+					},
 				},
 			},
 		},
