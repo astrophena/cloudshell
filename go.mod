2245d974b339015bb63714936960a7486c937484
Ilya Mateyko <me@astrophena.name>
2025-08-23 16:29:13 +0300

    don't use google.golang.org/api/cloudshell/v1 package
    It bloats the dependency tree.

--- go.mod
+++ go.mod
@@ -5,38 +5,17 @@ go 1.25
 require (
 	go.astrophena.name/base v0.10.0
 	golang.org/x/oauth2 v0.30.0
-	google.golang.org/api v0.248.0
 )
 
 require (
-	cloud.google.com/go/auth v0.16.5 // indirect
-	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
 	cloud.google.com/go/compute/metadata v0.8.0 // indirect
 	github.com/BurntSushi/toml v1.4.1-0.20240526193622-a339e1f7089c // indirect
-	github.com/felixge/httpsnoop v1.0.4 // indirect
-	github.com/go-logr/logr v1.4.3 // indirect
-	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/google/s2a-go v0.1.9 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/googleapis/enterprise-certificate-proxy v0.3.6 // indirect
-	github.com/googleapis/gax-go/v2 v2.15.0 // indirect
-	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
-	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
-	go.opentelemetry.io/otel v1.36.0 // indirect
-	go.opentelemetry.io/otel/metric v1.36.0 // indirect
-	go.opentelemetry.io/otel/trace v1.36.0 // indirect
-	golang.org/x/crypto v0.41.0 // indirect
 	golang.org/x/exp/typeparams v0.0.0-20231108232855-2478ac86f678 // indirect
 	golang.org/x/mod v0.26.0 // indirect
-	golang.org/x/net v0.43.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
-	golang.org/x/text v0.28.0 // indirect
 	golang.org/x/tools v0.35.0 // indirect
 	golang.org/x/tools/go/expect v0.1.1-deprecated // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250818200422-3122310a409c // indirect
-	google.golang.org/grpc v1.74.2 // indirect
-	google.golang.org/protobuf v1.36.7 // indirect
 	honnef.co/go/tools v0.6.0 // indirect
 )
 
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"errors"
 	"fmt"
 	"io/fs"
+	"net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -20,11 +21,10 @@ import (
 
 	"go.astrophena.name/base/cli"
 	"go.astrophena.name/base/logger"
+	"go.astrophena.name/base/request"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
-	cloudshell "google.golang.org/api/cloudshell/v1"
-	"google.golang.org/api/option"
 )
 
 func main() { cli.Main(new(app)) }
@@ -34,9 +34,9 @@ type app struct {
 	stateDir string
 
 	// initialized by Run
+	httpc       *http.Client
 	logf        logger.Logf
 	oauthConfig *oauth2.Config
-	svc         *cloudshell.Service
 }
 
 func (a *app) Run(ctx context.Context) error {
@@ -64,7 +64,7 @@ func (a *app) Run(ctx context.Context) error {
 		}
 		return err
 	}
-	a.oauthConfig, err = google.ConfigFromJSON(clientSecret, cloudshell.CloudPlatformScope)
+	a.oauthConfig, err = google.ConfigFromJSON(clientSecret, "https://www.googleapis.com/auth/cloud-platform")
 	if err != nil {
 		return err
 	}
@@ -72,10 +72,7 @@ func (a *app) Run(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	a.svc, err = cloudshell.NewService(ctx, option.WithTokenSource(a.oauthConfig.TokenSource(ctx, tok)))
-	if err != nil {
-		return err
-	}
+	a.httpc = a.oauthConfig.Client(ctx, tok)
 
 	if len(env.Args) == 0 {
 		return fmt.Errorf("%w: command is required, see -help for usage", cli.ErrInvalidArgs)
@@ -85,7 +82,7 @@ func (a *app) Run(ctx context.Context) error {
 
 	switch command {
 	case "info":
-		return a.info()
+		return a.info(ctx)
 	case "ssh":
 		return a.ssh(ctx)
 	case "start":
@@ -98,17 +95,17 @@ func (a *app) Run(ctx context.Context) error {
 		subargs := args[1:]
 		switch subcommand {
 		case "list":
-			return a.keyList()
+			return a.keyList(ctx)
 		case "add":
 			if len(subargs) == 0 {
 				return fmt.Errorf("%w: public key is required", cli.ErrInvalidArgs)
 			}
-			return a.keyAdd(subargs[0])
+			return a.keyAdd(ctx, subargs[0])
 		case "remove":
 			if len(subargs) == 0 {
 				return fmt.Errorf("%w: public key is required", cli.ErrInvalidArgs)
 			}
-			return a.keyRemove(subargs[0])
+			return a.keyRemove(ctx, subargs[0])
 		default:
 			return fmt.Errorf("%w: unknown subcommand %q for key", cli.ErrInvalidArgs, subcommand)
 		}
@@ -153,25 +150,57 @@ func (a *app) getToken(ctx context.Context) (*oauth2.Token, error) {
 	return newtok, nil
 }
 
-const envName = "users/me/environments/default"
+type environment struct {
+	// Full path to the Docker image used to run this environment, e.g. "gcr.io/dev-con/cloud-devshell:latest".
+	DockerImage string `json:"dockerImage"`
 
-func (a *app) info() error {
-	env, err := a.svc.Users.Environments.Get(envName).Do()
+	// Output only:
+
+	// Current execution state of this environment.
+	State string `json:"state"`
+	// Host to which clients can connect to initiate HTTPS or WSS connections with the environment.
+	WebHost string `json:"webHost"`
+	// Username that clients should use when initiating SSH sessions with the environment.
+	SSHUsername string `json:"sshUsername"`
+	// Host to which clients can connect to initiate SSH sessions with the environment.
+	SSHHost string `json:"sshHost"`
+	// Port to which clients can connect to initiate SSH sessions with the environment.
+	SSHPort int `json:"sshPort"`
+	// Public keys associated with the environment.
+	PublicKeys []string `json:"publicKeys"`
+}
+
+func (a *app) getEnvironment(ctx context.Context) (environment, error) {
+	return makeRequest[environment](ctx, a.httpc, http.MethodGet, "", nil)
+}
+
+func makeRequest[Response any](ctx context.Context, httpc *http.Client, method, url string, body any) (Response, error) {
+	const baseURL = "https://cloudshell.googleapis.com/v1/users/me/environments/default"
+	return request.Make[Response](ctx, request.Params{
+		Method:     method,
+		URL:        baseURL + url,
+		Body:       body,
+		HTTPClient: httpc,
+	})
+}
+
+func (a *app) info(ctx context.Context) error {
+	env, err := a.getEnvironment(ctx)
 	if err != nil {
 		return err
 	}
 
 	state := strings.ToLower(env.State)
-	state = lowercaseFirst(state) + "."
+	state = uppercaseFirst(state) + "."
 	a.logf(state)
 
 	a.logf("Docker image: %s", env.DockerImage)
 
-	if env.SshHost != "" && env.SshPort != 0 && env.SshUsername != "" {
+	if env.SSHHost != "" && env.SSHPort != 0 && env.SSHUsername != "" {
 		a.logf("SSH connection details:")
-		a.logf("  Host:     %s", env.SshHost)
-		a.logf("  Port:     %d", env.SshPort)
-		a.logf("  Username: %s", env.SshUsername)
+		a.logf("  Host:     %s", env.SSHHost)
+		a.logf("  Port:     %d", env.SSHPort)
+		a.logf("  Username: %s", env.SSHUsername)
 	} else {
 		a.logf("SSH is unavailable.")
 	}
@@ -179,17 +208,17 @@ func (a *app) info() error {
 	return nil
 }
 
-func lowercaseFirst(s string) string {
+func uppercaseFirst(s string) string {
 	if len(s) == 0 {
 		return s
 	}
 	runes := []rune(s)
-	runes[0] = unicode.ToLower(runes[0])
+	runes[0] = unicode.ToUpper(runes[0])
 	return string(runes)
 }
 
 func (a *app) ssh(ctx context.Context) error {
-	env, err := a.svc.Users.Environments.Get(envName).Do()
+	env, err := a.getEnvironment(ctx)
 	if err != nil {
 		return err
 	}
@@ -198,7 +227,7 @@ func (a *app) ssh(ctx context.Context) error {
 		if err := a.start(ctx); err != nil {
 			return err
 		}
-		env, err = a.svc.Users.Environments.Get(envName).Do()
+		env, err = a.getEnvironment(ctx)
 		if err != nil {
 			return err
 		}
@@ -208,7 +237,7 @@ func (a *app) ssh(ctx context.Context) error {
 }
 
 func (a *app) start(ctx context.Context) error {
-	if _, err := a.svc.Users.Environments.Start(envName, &cloudshell.StartEnvironmentRequest{}).Do(); err != nil {
+	if _, err := makeRequest[request.IgnoreResponse](ctx, a.httpc, http.MethodPost, ":start", struct{}{}); err != nil {
 		return err
 	}
 
@@ -218,7 +247,7 @@ func (a *app) start(ctx context.Context) error {
 	defer ticker.Stop()
 
 	for {
-		e, err := a.svc.Users.Environments.Get(envName).Do()
+		e, err := a.getEnvironment(ctx)
 		if err != nil {
 			return err
 		}
@@ -237,30 +266,23 @@ func (a *app) start(ctx context.Context) error {
 	}
 }
 
-func (a *app) sshExec(e *cloudshell.Environment) error {
-	if e.SshHost == "" || e.SshPort == 0 || e.SshUsername == "" {
+func (a *app) sshExec(e environment) error {
+	if e.SSHHost == "" || e.SSHPort == 0 || e.SSHUsername == "" {
 		return errors.New("ssh is unavailable")
 	}
 
-	host := e.SshUsername + "@" + e.SshHost
-	port := strconv.FormatInt(e.SshPort, 10)
+	host := e.SSHUsername + "@" + e.SSHHost
+	port := strconv.FormatInt(int64(e.SSHPort), 10)
 
-	path, err := exec.LookPath("ssh")
-	if err != nil {
-		return err
-	}
-
-	cmd := exec.Command(path, host, "-p", port, "-o", "StrictHostKeyChecking=no")
-
+	cmd := exec.Command("ssh", host, "-p", port, "-o", "StrictHostKeyChecking=no")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdin
-
 	return cmd.Run()
 }
 
-func (a *app) keyList() error {
-	e, err := a.svc.Users.Environments.Get(envName).Do()
+func (a *app) keyList(ctx context.Context) error {
+	e, err := a.getEnvironment(ctx)
 	if err != nil {
 		return err
 	}
@@ -277,23 +299,26 @@ func (a *app) keyList() error {
 	return nil
 }
 
-func (a *app) keyAdd(key string) error {
-	_, err := a.svc.Users.Environments.AddPublicKey(envName, &cloudshell.AddPublicKeyRequest{
+func (a *app) keyAdd(ctx context.Context, key string) error {
+	if _, err := makeRequest[request.IgnoreResponse](ctx, a.httpc, http.MethodPost, ":addPublicKey", struct {
+		Key string `json:"key"`
+	}{
 		Key: strings.TrimSuffix(key, "\n"),
-	}).Do()
-	if err != nil {
+	}); err != nil {
 		return err
 	}
-
 	a.logf("Public key added successfully.")
 	return nil
 }
 
-func (a *app) keyRemove(key string) error {
-	if _, err := a.svc.Users.Environments.RemovePublicKey(envName, &cloudshell.RemovePublicKeyRequest{Key: key}).Do(); err != nil {
+func (a *app) keyRemove(ctx context.Context, key string) error {
+	if _, err := makeRequest[request.IgnoreResponse](ctx, a.httpc, http.MethodPost, ":removePublicKey", struct {
+		Key string `json:"key"`
+	}{
+		Key: strings.TrimSuffix(key, "\n"),
+	}); err != nil {
 		return err
 	}
-
 	a.logf("Public key removed successfully.")
 	return nil
 }
